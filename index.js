3f9594f8db79d0eaaa2c87871334426841ef9eca
Milan <milan1adamovic@gmail.com>
2023-03-13 15:48:26 +0100

    Button event listener part1

---
+++ index.js
@@ -0,0 +1,8 @@
+var = numberOfDrumButtons = document.querySelectorAll(.drum).lenght;
+or (var i=0; i<
+
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("I got clicked");
+}
