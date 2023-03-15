dcb218b04ff5bf0e3d2bec7e1c3cbd0d16180c20
Milan <milan1adamovic@gmail.com>
2023-03-15 08:47:10 +0100

    Full JS function with working buttons and clicks, including picture background in css

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-var = numberOfDrumButtons = document.querySelectorAll(.drum).lenght;
-or (var i=0; i<
+// Store the audio files and their corresponding key codes in an object
+const sounds = {
+  w: new Audio("sounds/tom-1.mp3"),
+  a: new Audio("sounds/tom-2.mp3"),
+  s: new Audio("sounds/tom-3.mp3"),
+  d: new Audio("sounds/tom-4.mp3"),
+  j: new Audio("sounds/snare.mp3"),
+  k: new Audio("sounds/crash.mp3"),
+  l: new Audio("sounds/kick-bass.mp3"),
+};
 
-document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick() {
-  alert("I got clicked");
+// Function to play the corresponding sound for a given key
+function playSound(key) {
+  if (sounds[key]) {
+    sounds[key].currentTime = 0;
+    sounds[key].play();
+  }
 }
+
+// Add event listeners to the buttons and document for keyboard presses
+const buttons = document.querySelectorAll(".drum");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    const buttonKey = this.textContent;
+    playSound(buttonKey);
+  });
+}
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key;
+  playSound(key);
+});
